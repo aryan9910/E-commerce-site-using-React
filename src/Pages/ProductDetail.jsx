06ac2a0e7b5e8a5db522b83209058d5b3636a414
aryan9910 <aryanj9910@gmail.com>
2025-06-21 21:24:31 +0530

    changes

---
+++ src/Pages/ProductDetail.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { useParams } from 'react-router-dom';
+
+const ProductDetail = () => {
+    const {id}= useParams()
+  return (
+    <h1>
+        Product Detail Page - ID: {id} 
+    </h1>
+  )
+}
+
+export default ProductDetail
